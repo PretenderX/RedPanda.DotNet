4741b255820cf6ca4dcd1d220fcb6baf7442f147
Lionel Wu <lionel.wuliang@gmail.com>
2020-05-29 14:44:16 +0800

    support netcoreapp3.0

--- src/RedPanda.Service.Governance/Common/ServiceGovernanceConfigProvider.cs
+++ src/RedPanda.Service.Governance/Common/ServiceGovernanceConfigProvider.cs
@@ -1,10 +1,16 @@
-﻿using RedPanda.Service.Governance.Exceptions;
+﻿#if NETSTANDARD
 using System.Configuration;
+#endif
+#if NETCOREAPP
+using Microsoft.Extensions.Configuration;
+#endif
+using RedPanda.Service.Governance.Exceptions;
 
 namespace RedPanda.Service.Governance.Common
 {
     public class ServiceGovernanceConfigProvider : IServiceGovernanceConfigProvider
     {
+#if NETSTANDARD2_0
         public string GetAppSetting(string name, bool isOptional = true)
         {
             var value = ConfigurationManager.AppSettings.Get(name);
@@ -16,5 +22,27 @@ namespace RedPanda.Service.Governance.Common
 
             return value;
         }
+#endif
+
+#if NETCOREAPP3_0
+        protected readonly IConfiguration Configuration;
+
+        public ServiceGovernanceConfigProvider(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public virtual string GetAppSetting(string name, bool isOptional = true)
+        {
+            var value = Configuration[name];
+
+            if (!isOptional && string.IsNullOrEmpty(value))
+            {
+                throw new NotFoundServiceGovernanceConfigItemException(name);
+            }
+
+            return value;
+        }
+#endif
     }
 }
--- src/RedPanda.Service.Governance/ServiceGovernanceConfig.cs
+++ src/RedPanda.Service.Governance/ServiceGovernanceConfig.cs
@@ -4,7 +4,12 @@ namespace RedPanda.Service.Governance
 {
     public static class ServiceGovernanceConfig
     {
+#if NETSTANDARD2_0
         public static IServiceGovernanceConfigProvider ServiceGovernanceConfigProvider { get; set; } = new ServiceGovernanceConfigProvider();
+#endif
+#if NETCOREAPP3_0
+        public static IServiceGovernanceConfigProvider ServiceGovernanceConfigProvider { get; set; }
+#endif
 
         public static IJsonProvider JsonProvider { get; set; } = new SystemTextJsonProvider();
     }
