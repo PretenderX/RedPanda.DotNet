7923d55bf704f42bf8d454eac0c1b1bce1c4e2a5
PretenderX <pretenderx@outlook.com>
2022-11-02 16:20:32 +0800

    Add IServiceProvider.UseServiceGovermance to register ApplicationStarted and ApplicationStopping events for register/deregistger self service.

--- src/RedPanda.Service.Governance/DependencyInjection/ServiceGovermanceExtension.cs
+++ src/RedPanda.Service.Governance/DependencyInjection/ServiceGovermanceExtension.cs
@@ -1,22 +1,36 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using RedPanda.Service.Governance.Common;
 using RedPanda.Service.Governance.Configuration;
 using RedPanda.Service.Governance.Discovery;
 using RedPanda.Service.Governance.Registration;
+using System;
+using System.Collections.Generic;
 
 namespace RedPanda.Service.Governance.DependencyInjection
 {
     public static class ServiceGovermanceExtension
     {
-        public static IServiceCollection AddServiceGovermance(this IServiceCollection serviceCollection, IConfiguration configuration)
+        public static IServiceCollection AddServiceGovermance(this IServiceCollection serviceCollection)
         {
-            ServiceGovernanceConfig.ServiceGovernanceConfigProvider = new ServiceGovernanceConfigProvider(configuration);
             serviceCollection.AddSingleton<IServiceRegistration, ServiceRegistration>();
             serviceCollection.AddSingleton<IServiceDiscovery, ServiceDiscovery>();
             serviceCollection.AddSingleton<IScopedConfigManager, ScopedConfigManager>();
 
             return serviceCollection;
+        }
+
+        public static IServiceProvider UseServiceGovermance(this IServiceProvider serviceProvider, Action<Dictionary<string, string>> appendMetaAction = null)
+        {
+            ServiceGovernanceConfig.ServiceGovernanceConfigProvider = new ServiceGovernanceConfigProvider(serviceProvider.GetRequiredService<IConfiguration>());
+
+            var hostApplicationLifetime = serviceProvider.GetService<IHostApplicationLifetime>();
+
+            hostApplicationLifetime?.ApplicationStarted.Register(() => serviceProvider.GetService<IServiceRegistration>()?.RegisterSelf(appendMetaAction));
+            hostApplicationLifetime?.ApplicationStopping.Register(() => serviceProvider.GetService<IServiceRegistration>()?.DeregisterSelf());
+
+            return serviceProvider;
         }
     }
 }
